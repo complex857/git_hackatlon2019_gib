f0142903b2977321add178761a048caea92cb652
David Szabo <david.szabo@betvictor.com>
2019-10-27 12:55:51 +0100

    b0rked leveling

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -2,9 +2,12 @@ import React, { Component, Fragment } from 'react';
 import { AsyncStorage } from 'react-native';
 import { StyleSheet, Text, View, SafeAreaView, SectionList, ScrollView } from 'react-native';
 import { withNavigationFocus } from 'react-navigation';
+import "react-step-progress-bar/styles.css";
+import { ProgressBar, Step } from "react-step-progress-bar";
 import CustomHeader from '../components/CustomHeader';
 import defaultTasks from '../assets/jsons/diddits-base';
 import commonStyles from '../styles/Common';
+
 
 const findTask = (name, tasks) => {
   return tasks.find(task => task.name === name);
@@ -30,13 +33,32 @@ class SettingsScreen extends Component {
     this.state = {
       tasksDone: null,
       tasks: null,
+      level: 1,
     }
+  }
+
+  nextLevelXp = (level) => {
+    return (3 * (level ^ 5)) / 4;
   }
 
   fetchData = () => {
     AsyncStorage.getItem("tasks_done").then(tasksDone => {
       tasksDone = JSON.parse(tasksDone || '[]')
       this.setState({ tasksDone })
+
+      AsyncStorage.getItem("level").then(level => {
+        level = parseInt(level, 10) || 1;
+        this.setState({ level })
+
+        const checkLevelUp = () => {
+          while (this.totalPointsValue() > this.nextLevelXp(level)) {
+            level++
+            this.setState({level: level })
+            AsyncStorage.setItem("level", level);
+          }
+        }
+        checkLevelUp();
+      });
     });
     AsyncStorage.getItem("tasks").then(tasks => {
       tasks = [...defaultTasks, ...JSON.parse(tasks || '[]')]
@@ -51,17 +73,20 @@ class SettingsScreen extends Component {
     ];
   }
 
-  totalPoints() {
-    const totalPoints = this.state.tasksDone.reduce((acc, td) => {
+  totalPointsValue = () => {
+    return this.state.tasksDone.reduce((acc, td) => {
       const task = findTask(td.task, this.state.tasks)
       if (task) {
         return acc + task.points;
       }
       return acc;
     }, 0);
+  }
+
+  totalPoints() {
     return (
       <Text style={commonStyles.p}>
-        The total points you earned: <Text style={{fontWeight: 'bold'}}>{totalPoints}</Text>
+        The total points you earned: <Text style={{fontWeight: 'bold'}}>{this.totalPointsValue()}</Text>
       </Text>
     );
   }
@@ -95,8 +120,34 @@ class SettingsScreen extends Component {
       return null;
     }
 
+    const percent = ((this.totalPointsValue()) / this.nextLevelXp(this.state.level) ) * 100;
+
     return (
       <SafeAreaView style={commonStyles.container}>
+
+        <View style={{width: 320, height: 50, paddingTop: 30}}>
+          <ProgressBar percent={percent} fillBackground="#d0e562">
+            <Step transition="scale">
+            {({ accomplished }) => (
+              <img
+                style={{ filter: `grayscale(${accomplished ? 0 : 80}%)` }}
+                width="30"
+                src="https://vignette.wikia.nocookie.net/pkmnshuffle/images/9/9d/Pichu.png/revision/latest?cb=20170407222851"
+              />
+            )}
+          </Step>
+          <Step transition="scale">
+            {({ accomplished }) => (
+              <img
+                style={{ filter: `grayscale(${accomplished ? 0 : 80}%)` }}
+                width="30"
+                src="https://orig00.deviantart.net/493a/f/2017/095/5/4/raichu_icon_by_pokemonshuffle_icons-db4ryym.png"
+              />
+            )}
+          </Step>
+        </ProgressBar>
+        </View>
+
         <ScrollView style={commonStyles.contentContainer}>
           {this.totalPoints()}
           {this.state.tasksDone.length === 0 ? this.noTasksDone() : this.tasksDone() }
